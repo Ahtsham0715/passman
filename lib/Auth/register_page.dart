89d7ea279beb488229dc5aeba88d04f5a64424f9
Ahtsham0715 <ahtsham50743@gmail.com>
2022-10-06 19:26:41 +0500

    register page added

--- lib/Auth/register_page.dart
+++ lib/Auth/register_page.dart
@@ -1,28 +1,34 @@
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
-import 'package:passman/Auth/register_page.dart';
 import 'package:passman/constants.dart';
 import 'package:passman/custom_formfield.dart';
 import 'package:passman/widgets/custom_shape.dart';
 
-class LoginPage extends StatefulWidget {
-  const LoginPage({super.key});
+class RegisterPage extends StatefulWidget {
+  const RegisterPage({super.key});
 
   @override
-  State<LoginPage> createState() => _LoginPageState();
+  State<RegisterPage> createState() => _RegisterPageState();
 }
 
-class _LoginPageState extends State<LoginPage> {
+class _RegisterPageState extends State<RegisterPage> {
   GlobalKey<FormState> _formkey = GlobalKey<FormState>();
   TextEditingController _email = TextEditingController();
   TextEditingController _password = TextEditingController();
+  TextEditingController _confirmpassword = TextEditingController();
 
   bool passwordVisible = true;
+  bool confirmPasswordVisible = true;
   // Visibility of password
   void _passwordVisibility() {
     setState(() {
       passwordVisible = !passwordVisible;
+    });
+  }
+
+  void _confirmPasswordVisibility() {
+    setState(() {
+      confirmPasswordVisible = !confirmPasswordVisible;
     });
   }
 
@@ -35,6 +41,12 @@ class _LoginPageState extends State<LoginPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       resizeToAvoidBottomInset: false,
+      appBar: AppBar(
+        foregroundColor: Colors.black,
+        backgroundColor: Colors.transparent,
+        elevation: 0.0,
+        toolbarHeight: 30.0,
+      ),
       body: Container(
         // padding: const EdgeInsets.only(top: 15.0),
         width: MediaQuery.of(context).size.width,
@@ -45,13 +57,13 @@ class _LoginPageState extends State<LoginPage> {
           key: _formkey,
           child: Column(
             children: [
-              SizedBox(
-                height: MediaQuery.of(context).size.height * 0.06,
-              ),
+              // SizedBox(
+              //   height: MediaQuery.of(context).size.height * 0.06,
+              // ),
               const Padding(
-                padding: EdgeInsets.symmetric(vertical: 10.0),
+                padding: EdgeInsets.symmetric(vertical: 2.0),
                 child: Text(
-                  'Login',
+                  'Register',
                   style: TextStyle(
                     fontSize: 40.0,
                     fontWeight: FontWeight.w500,
@@ -63,7 +75,7 @@ class _LoginPageState extends State<LoginPage> {
                 textAlign: TextAlign.center,
                 text: TextSpan(children: [
                   const TextSpan(
-                      text: 'By signing in you are agreeing\nour ',
+                      text: 'By signing up you are agreeing\nour ',
                       style: TextStyle(
                         fontSize: 22.0,
                         color: Colors.black,
@@ -152,45 +164,42 @@ class _LoginPageState extends State<LoginPage> {
               SizedBox(
                 height: responsiveHW(context, ht: 3),
               ),
-              ButtonBar(
-                alignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.only(left: 15.0),
-                    child: TextButton(
-                      onPressed: () {
-                        Get.to(
-                          () => const RegisterPage(),
-                        );
-                      },
-                      child: const Text(
-                        'Register',
-                        style: TextStyle(
-                          fontSize: 21.0,
-                          fontWeight: FontWeight.w500,
-                          fontFamily: 'majalla',
-                        ),
-                      ),
-                    ),
+              customTextField(
+                "Confirm Password",
+                confirmPasswordVisible,
+                IconButton(
+                  icon: Icon(
+                    //choose the icon on based of passwordVisibility
+                    confirmPasswordVisible
+                        ? Icons.visibility_outlined
+                        : Icons.visibility_off_outlined,
+                    color: Colors.grey,
                   ),
-                  Padding(
-                    padding: const EdgeInsets.only(right: 15.0),
-                    child: TextButton(
-                      onPressed: () {},
-                      child: const Text(
-                        'Forgot Password',
-                        style: TextStyle(
-                          fontSize: 21.0,
-                          fontWeight: FontWeight.w500,
-                          fontFamily: 'majalla',
-                        ),
-                      ),
-                    ),
+                  onPressed: _confirmPasswordVisibility,
+                ),
+                _confirmpassword,
+                (value) {
+                  if (value!.isEmpty) {
+                    return "Please Re-Enter Your Password";
+                  }
+                  if (value != _password.text) {
+                    return "Both Password Should Be Matched";
+                  }
+                },
+                (value) {
+                  _confirmpassword.text = value!;
+                },
+                responsiveHW(context, wd: 100),
+                responsiveHW(context, ht: 100),
+                const UnderlineInputBorder(
+                  borderSide: BorderSide(
+                    color: Colors.grey,
                   ),
-                ],
+                ),
+                pIcon: Icons.lock_outline_rounded,
               ),
               SizedBox(
-                height: MediaQuery.of(context).size.height * 0.025,
+                height: responsiveHW(context, ht: 8),
               ),
               MaterialButton(
                 onPressed: () {},
@@ -201,7 +210,7 @@ class _LoginPageState extends State<LoginPage> {
                 minWidth: MediaQuery.of(context).size.width * 0.8,
                 height: MediaQuery.of(context).size.height * 0.055,
                 child: const Text(
-                  'Login',
+                  'Register',
                   style: TextStyle(
                       fontSize: 25.0,
                       fontWeight: FontWeight.w500,
@@ -209,10 +218,9 @@ class _LoginPageState extends State<LoginPage> {
                       fontFamily: 'majalla'),
                 ),
               ),
-
-              SizedBox(
-                height: MediaQuery.of(context).size.height * 0.03,
-              ),
+              // SizedBox(
+              //   height: MediaQuery.of(context).size.height * 0.05,
+              // ),
               Expanded(
                 child: Stack(
                   fit: StackFit.passthrough,
@@ -230,47 +238,9 @@ class _LoginPageState extends State<LoginPage> {
                               .toDouble()), //You can Replace [WIDTH] with your desired width for Custom Paint and height will be calculated automatically
                       painter: CustomAuthShape(),
                     ),
-                    Positioned(
-                      bottom: 50.0,
-                      child: MaterialButton(
-                        onPressed: () {},
-                        color: const Color(0xFF0386D0),
-                        elevation: 0.0,
-                        shape: RoundedRectangleBorder(
-                            side: BorderSide(
-                              width: 2.0,
-                              color: Colors.white.withOpacity(0.8),
-                            ),
-                            borderRadius: BorderRadius.circular(10.0)),
-                        minWidth: 5,
-                        padding: const EdgeInsets.all(1.0),
-                        height: MediaQuery.of(context).size.height * 0.095,
-                        child: const Icon(
-                          Icons.fingerprint,
-                          color: Colors.white,
-                          size: 60.0,
-                        ),
-                      ),
-                    ),
-                    Positioned(
-                      bottom: 0.0,
-                      child: Text(
-                        'Login with touch ID',
-                        style: TextStyle(
-                          fontSize: 21.0,
-                          fontWeight: FontWeight.w500,
-                          fontFamily: 'majalla',
-                          color: Colors.white.withOpacity(0.85),
-                        ),
-                      ),
-                    ),
                   ],
                 ),
               ),
-              // SizedBox(
-              //     width: MediaQuery.of(context).size.width * 0.8,
-              //     height: MediaQuery.of(context).size.height * 0.1,
-              //     child: Image.asset('assets/auth.png')),
             ],
           ),
         ),
